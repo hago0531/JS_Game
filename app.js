084f79493dae2a41396aff056ca611e88cd1c006
hago0531 <hago0531@naver.com>
2020-04-21 21:12:09 +0900

    Add_Context

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 let painting = false;
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function stopPainting() {
   painting = false;
 }
 
-function onMouseDown(event) {
+function startPainting(event) {
   painting = true;
 }
 
 function onMouseDown(event) {
-  stopPainting();
+  painting = true;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
-  canvas.addEventListener("mouseleave", stopPainting(););
+  canvas.addEventListener("mousedown", startPainting());
+  canvas.addEventListener("mouseup", stopPainting());
+  canvas.addEventListener("mouseleave", stopPainting());
 }
